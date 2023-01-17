20094b12f138052f56aaade47fd4c37eb96d6726
Mateus Wesley <mateuswesley314@gmail.com>
2023-01-17 11:06:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const data = new Date()
-let tempo_inicial = data.getTime();
 
 // botoes
 let play_button = document.querySelector('.play')
@@ -14,6 +12,7 @@ let textoSegundos = document.querySelector('span.segundos')
 let segundosInicial = Number(textoSegundos.innerText)
 let minutos
 
+
 /* Métodos*/
 function toggleBotao(botao1, botao2){
     botao1.classList.toggle('hide')
@@ -25,68 +24,11 @@ function formatarTempo(tempo){
 }
 
 
-function playTimer(){
-    const data_inicial = new Date()
-    let tempo_inicial = data.getTime()/1000
-
-    while (Number(texto_tempo)<60){
-        const data_final = new Date()
-        let tempo_final = data.getTime()/1000
-
-        if((tempo_final-tempo_inicial)>=1){
-           texto_tempo.innerText = texto_tempo.value+1
-        }
-    }
-}
-
-// criar uma data com o numero de minutos do timer
-// para calcular quanto tempo falta para chegar ate ela
-function dateAfter(){
-    let minutos_timer = Number(textoMinutos.innerText)
-    let segundos_timer = Number(textoSegundos.innerText)
-
-    let data_agora = new Date()
-    let data_depois = new Date(data_agora)
-    data_depois.setMinutes(data_agora.getMinutes()+minutos_timer)
-    data_depois.setSeconds(data_agora.getSeconds()+segundos_timer)
-    data_depois.setMilliseconds(data_agora.getMilliseconds() + 40)
-    return data_depois
-}
-
-function countDown(later){
-    const agora = new Date()
-
-    const intervalo_segundos = (later - agora)/1000
-
-    const num_minutos = Math.floor(intervalo_segundos/60)%60
-
-    const num_segundos = Math.floor(intervalo_segundos)%60
-
-    textoMinutos.innerText = formatarTempo(num_minutos)
-    textoSegundos.innerText = formatarTempo(num_segundos)
-
-    minutos_timer = Number(textoMinutos.innerText)
-    segundos_timer = Number(textoSegundos.innerText)
-
-    if(minutos_timer == 0 && segundos_timer == 0){
-        clearInterval(myInterval)
-    }
-}
-
 /*Eventos*/
 /*[button.botao.play, button.botao.pause.hide, button.botao.stop, button.botao.timer.hide, button.botao.musica-on, button.botao.musica-off]*/
 
 // Botao de play
 play_button.addEventListener('click', function(){
-    date_after = dateAfter()
-
-    myInterval = setInterval(function(){
-        countDown(date_after)
-    }, 1000)
-
-    let minutos_timer = Number(textoMinutos.innerText)
-    let segundos_timer = Number(textoSegundos.innerText)
-
     toggleBotao(play_button, pause_button)
     stop_button.classList.remove('hide')
     timer_button.classList.add('hide')
@@ -95,16 +37,13 @@ play_button.addEventListener('click', function(){
 
 pause_button.addEventListener('click', function(){
     toggleBotao(play_button, pause_button)
-    clearInterval(myInterval);
+
 })
 
 stop_button.addEventListener('click', function(){
     toggleBotao(stop_button, timer_button)
     play_button.classList.remove('hide')
     pause_button.classList.add('hide')
-    clearInterval(myInterval);
-    textoMinutos.innerText = formatarTempo(minutosInicial)
-    textoSegundos.innerText = formatarTempo(segundosInicial)
 })
 
 timer_button.addEventListener('click', function(){
